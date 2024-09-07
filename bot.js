90f16879ec13c1453adfddcac9c2fbb72df5d295
bomie <bomidelrey@gmail.com>
2024-09-07 10:15:04 +0700

    bot.js

--- bot.js
+++ bot.js
@@ -6,10 +6,10 @@ const { Classic } = require("musicard");
 const { prefix } = require('./config.json');
 const nodes = [
     {
-        host: "lava-v3.ajieblogs.eu.org",
-        password: "https://dsc.gg/ajidevserver",
-        port: 443,
-        secure: true
+        host: "37.114.42.191",
+        port: 9906, 
+        password: "danteisnttaken", 
+         secure: false
     },
 ];
 
